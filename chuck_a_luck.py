a26cc5f965c45a7ba71758775e1b2a3044970c83
Heather McMillan <heather.o.mcmillan@gmail.com>
2021-05-15 21:18:06 -0500

    update with a few more questions answered

--- chuck_a_luck.py
+++ chuck_a_luck.py
@@ -20,35 +20,42 @@ import numpy as np
 import pandas as pd
 die = [1, 2, 3, 4, 5, 6]
 rolls = ncols = 3
-trials = nrows = 10_000
+trials = nrows = 1_00_000
 sim1 = np.random.choice(die, rolls*trials).reshape(nrows, ncols)
 # What are your chances of getting an outcome payout of $2? (where we earned $3 on the $1 ante)
 # should be close to 1/36 which is a .27% chance (the theoretical probability)
 # we're going to guess 1 as our number
 # to check sum of all elements needs to be 3
-(sim1.sum(axis = 1) == 3).mean()
-# ------ 0.39%  chance 
+win2 = (sim1.sum(axis = 1) == 3).mean()
+# ------ 0.45%  chance 
 
 # What are your chances of getting an payout of $1?
-# here use 1 as my guess, use for loop to to go through array and put True if 1 is in the row False if it's not
-# Try to do this with list comp! <-------
-chancelist = []
-for row in sim1:
-    if 1 in row:
-        chancelist.append(True)
-    else:
-        chancelist.append(False)
+# create true false values for 1s 
+rows_with_ones = (sim1 == 1) 
+# count up the true values
+true_values = rows_with_ones.sum(axis = 1)
+# if true value count is == 2 for rolling exactly 2 ones return true
+# this is all the trials where we rolled exactly 2 ones
+true_values == 2
+# take mean of that return
+win1 = (true_values == 2).mean()
 
-# turn list into np array and take the mean to get percentage
-np.array(chancelist).mean()
 
-#------ 42% chance of getting a payout of 1
+#------ ~6.88% chance of getting a payout of 1 (rolling 2 of the same die)
 
 # pd.DataFrame(rolls).apply(lambda row: 3 in row.values, axis =1).mean()
 
 # What are your chances of getting a payout of $0 where you ante $1 and win $1?
+# Do the same type of check as the question above use 1 true value this time
+true_values == 1
+win0 = (true_values == 1).mean()
+
+# ------ ~34.6% chance of ggetting a payout of $0
 
 # What are your chances of getting a payout of -$1 where you ante up, but don't win anything?
+true_values == 0
+lose = (true_values == 0).mean()
+# ------ ~58.08% of losing money
 
 # What is the average cost/gain per game? (think of averaging the total payout calculation across all simulations)
 
